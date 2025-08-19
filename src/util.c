4f8dc175d6d140f7af7605b901f3883459a5cdf2
bbbbbr <bbbbbr@users.noreply.github.com>
2025-08-18 23:04:53 -0700

    Util: update apa exit

--- src/util.c
+++ src/util.c
@@ -1,8 +1,9 @@
 #include <gbdk/platform.h>
 #include <stdint.h>
 
-void apa_exit(void) {
-    set_interrupts(IE_REG & ~LCD_IFLAG);
+#define M_RESET_TO_DEFAULT 0u
+
+void apa_exit(void){
 
     // Remove the two APA interrupt routines
     // Turn off LCD Interrupt
@@ -12,15 +13,18 @@ void apa_exit(void) {
     // Remove the APA LCD interrupt routine (with interrupts turned off)
     CRITICAL {
         __asm \
-            push BC
-            push HL
+            push DE
 
-            LD      BC, #.drawing_lcd
-            LD      HL, #.int_0x48
-            CALL    .remove_int
+            LD      DE, #.drawing_lcd
+            CALL    .remove_LCD
 
-            pop HL
-            pop BC
+            pop DE
         __endasm;
     }
+
+    // Reset VRAM to use the 8000 range again
+    LCDC_REG &= ~LCDCF_BG8000;
+
+    //sets drawing flag to OFF
+    mode(M_RESET_TO_DEFAULT);
 }
